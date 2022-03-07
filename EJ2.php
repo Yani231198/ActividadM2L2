939c1114f57537801ff28b3561a78d6265f8ebc5
Yani231198 <99046276+Yani231198@users.noreply.github.com>
2022-03-06 18:57:19 -0600

    Add files via upload

---
+++ EJ2.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <?php require_once "menu.php" ?>
+    <title>EJ2</title>
+  </head>
+  <body>
+  <div class="container"><br>
+    <div class="row justify-content-center">
+        <div class="col-6 p-5 bg-white shadow-lg rounded">
+        <h3>Ejercicio 2</h3>
+            <hr>
+            <br>
+            <?php 
+                $pesoVehiculo = 80;
+                $pesoPermitido = 10;
+
+                if ($pesoVehiculo >= $pesoPermitido) 
+                {
+                    echo "El vehiculo esta permitido";
+                }
+                else 
+                {
+                    echo "El peso maximo del vehiculo es: ".$pesoVehiculo."Siendo ".$pesoPermitido." el peso maximo permitido";
+                } 
+            ?>
+        </div>
+    </div>
+  </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ Menu.php
@@ -0,0 +1,44 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Programacion Web</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="EJ1.php">Ejercicio 1</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="EJ2.php">Ejercicio 2</a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">Ejercicio 3</a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="EJ3.php">Ejercicio 3 V1</a></li>
+                        <li><a class="dropdown-item" href="EJ3V2.php">Ejercicio 3 V2</a></li>
+                    </ul>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">Ejercicio 4</a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="EJ4.php">Ejercicio 4 V1</a></li>
+                        <li><a class="dropdown-item" href="EJ4V2.php">Ejercicio 4 V2</a></li>
+                    </ul>
+                </li>
+
+            </ul>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-light" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+</nav>
